5981aa5600cde51888f80811ba1ae762b31047e7
JUN GYEWON <tonyjev93@gmail.com>
2022-01-11 14:24:03 +0900

    init

---
+++ src/test/java/com/nhn/nhnsoft/jobray/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.nhnsoft.jobray.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
